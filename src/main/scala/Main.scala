386ba122414a6c971596102ae8e5915a94e2e0ce
Peter Emil Jensen <ubs908@gmail.com>
2017-04-28 14:26:30 +0200

    Added Node initialization to main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,9 @@
 package example
 
-import dk.diku.blockchain.models._
-import dk.diku.blockchain.actors._
+import dk.diku.blockchain._
 
 object Main extends App {
+
+  val node = new Node(9000)
 
 }
