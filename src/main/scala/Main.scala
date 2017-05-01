629b47558f23c535489e53e7c1a9b7e276a966cd
Peter Emil Jensen <ubs908@gmail.com>
2017-05-01 12:47:49 +0200

    Added print for blocklength

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,9 +7,11 @@ object Main extends App {
   val node: Node = new Node(9000)
 
   while (true) {
+
     val args = scala.io.StdIn.readLine().split(" ")
     args match {
       case Array("loaf", data) => println(do_loaf(data))
+      case Array("print", "blocklength") => println(node.getLength)
       case _ => println("*** unknown function")
     }
 
