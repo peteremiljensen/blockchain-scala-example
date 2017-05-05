4081bfd0bcb34f1d6779fc1ca9cc8949d82818b3
Peter Emil Jensen <ubs908@gmail.com>
2017-05-05 10:48:34 +0200

    Made consensuschecks and more realistic loaf and block checks, proof of work

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,24 @@ import dk.diku.blockchain._
 
 object Main extends App {
 
-  implicit val validator = Validator((l: Loaf) => true, (b: Block) => true)
+  def loafVal(l: Loaf) =
+    l.calculateHash == l.hash
+
+  def blockVal(b: Block) =
+    b.calculateHash == b.hash && b.hash.substring(0,4) == "0000"
+
+  def consensusCheck(localLength: Integer, recLength: Integer) =
+    localLength < recLength
+
+  def consensus(localChain: List[Block], recChain: List[Block]) =
+    if (localChain.length > recChain.length)
+      localChain
+    else
+      recChain
+
+  implicit val validator = Validator(loafVal, blockVal,
+    consensusCheck, consensus)
+
   implicit val system = ActorSystem()
   val port: Integer = if (args.length > 0) args(0).toInt else 9000
   val node: Node = new Node(port)
