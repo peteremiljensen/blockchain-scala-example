ca25d1da650486e29457a8d5ab3cacbeba8bd751
Peter Emil Jensen <ubs908@gmail.com>
2017-05-10 12:11:47 +0200

    Now quits gracefully

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -46,31 +46,36 @@ object Main extends App {
     "000077dbf86e9c0d593ac746a0658d88b966ddd0a132dcf9294c23a929ed4573")
 
   if (node.addBlock(genesisBlock)) {
+    print("(freechain) ")
     while (true) {
-      print("(freechain) ")
-      val args = scala.io.StdIn.readLine().split(" ")
-      args match {
-        case Array("loaf", data) =>
-          node.addLoaf(Loaf.generateLoaf(data))
-        case Array("mine") =>
-          (node.getLoaves(1000), node.getLength) match {
+      val in = scala.io.StdIn.readLine()
+      if (in != null) {
+        print("(freechain) ")
+        val args = in.split(" ")
+        args match {
+          case Array("loaf", data) =>
+            node.addLoaf(Loaf.generateLoaf(data))
+          case Array("mine") =>
+            (node.getLoaves(1000), node.getLength) match {
 
-            case (Right(loaves: Seq[Loaf] @unchecked), Right(length: Int)) =>
-              node.getBlock(length-1) match {
+              case (Right(loaves: Seq[Loaf] @unchecked), Right(length: Int)) =>
+                node.getBlock(length-1) match {
 
-                case Right(Some(block: Block)) =>
-                  if (!node.addBlock(mine(loaves, block)))
-                    println("*** error adding mined block")
-                case _ => println("*** error getting block")
-              }
-            case _ => println("*** error getting chain info")
-          }
-        case Array("print", "blocklength") => println(node.getLength)
-        case Array("print", "blockchain") => println(node.getChain)
-        case Array("print", "loafpool") => println(node.getLoaves(20))
-        case Array("connect", ip, port) => node.connect(ip, port.toInt)
-        case Array("") =>
-        case _ => println("*** unknown function")
+                  case Right(Some(block: Block)) =>
+                    if (!node.addBlock(mine(loaves, block)))
+                      println("*** error adding mined block")
+                  case _ => println("*** error getting block")
+                }
+              case _ => println("*** error getting chain info")
+            }
+          case Array("print", "blocklength") => println(node.getLength)
+          case Array("print", "blockchain") => println(node.getChain)
+          case Array("print", "loafpool") => println(node.getLoaves(20))
+          case Array("connect", ip, port) => node.connect(ip, port.toInt)
+          case Array("quit") | Array("exit") => node.exit; System.exit(0)
+          case Array("") =>
+          case _ => println("*** unknown function")
+        }
       }
     }
   } else {
